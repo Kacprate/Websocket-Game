ec40c8620d4848f718557e6c6fe85a64700bed2e
Kacprate <kacpratecontact@gmail.com>
2021-04-02 19:57:05 +0200

    python websocketapp, command handling, upgraded message handling

--- modules/client.py
+++ modules/client.py
@@ -1,40 +1,81 @@
 import asyncio
+from threading import Thread
 
-import websockets
+import websocket
 
 import modules.logger as logger
 import modules.message as message_module
 
 logger = logger.get_logger(__name__)
+connected = False
 
-async def message(websocket_url):
-    async with websockets.connect(websocket_url) as socket:
-        while True:
-            msg = input("Message: ")
+def send_message(websocket, msg):
+    # msg = input("Message: ")
+    if msg == '':
+        return
 
-            if msg == '/end':
-                message = message_module.Message('CONNECTION_CLOSE', msg)
-            elif msg.startswith('/setname'):
-                data = msg.split(' ')
-                name = ' '.join(data[1:])
-                message = message_module.Message('SET_NAME', name)
-            else:
-                message = message_module.Message('TEXT', msg)
+    if msg == '/end':
+        message = message_module.Message('CONNECTION_CLOSE', msg)
+    elif msg.startswith('/setname'):
+        message = message_module.Message('TEXT', msg)
+    else:
+        message = message_module.Message('TEXT', msg)
 
-            await socket.send(message.pack())
+    websocket.send(message.pack())
 
-            if message.message_type == 'CONNECTION_CLOSE':
-                break
+    if message.message_type == 'CONNECTION_CLOSE':
+        print('Connection closed, press enter to continue.')
+        websocket.close()
+        return
 
-            packed_response = await socket.recv()
-            response = message_module.Message.from_packed(packed_response)
+def on_message(websocket, packed_response):
+    if packed_response is None:
+        return
 
-            if response.message_type == 'TEXT':
-                print(response.data)
-            elif response.message_type == 'STATUS':
-                print(response.data)
-            
+    response = message_module.Message.from_packed(packed_response)
+
+    if response.message_type == 'TEXT':
+        logger.info(response.data)
+    elif response.message_type == 'STATUS':
+        logger.info(response.data)
+
+def on_open(websocket):
+    global connected
+    connected = True
+    websocket.send(message_module.Message('HELLO', "").pack())
+
+def on_error(websocket, error_msg):
+    logger.error(error_msg)
+
+def on_close(websocket):
+    global connected
+    connected = False
+    raise Exception('websocket closed, do something')
+
+def game_thread(app):
+    while True:
+        message = input()
+        
+        if not connected:
+            break
+
+        try:
+            send_message(app, message)
+        except websocket._exceptions.WebSocketConnectionClosedException as e:
+            logger.info('Connection is closed')
+            break
+        except Exception as e:
+            raise e
 
 def run(ip, port):
     websocket_url = f'ws://{ip}:{port}'
-    asyncio.get_event_loop().run_until_complete(message(websocket_url=websocket_url))
+    app = websocket.WebSocketApp(websocket_url, 
+                                 on_message=on_message,
+                                 on_error=on_error,
+                                 on_close=on_close,
+                                 on_open=on_open)
+
+    thread = Thread(target=app.run_forever)
+    thread.start()
+
+    game_thread(app)
--- modules/server.py
+++ modules/server.py
@@ -11,25 +11,53 @@ logger = logger.get_logger(__name__)
 
 connected_clients = {}
 
+command_prefix = '/'
+
 class Status(Enum):
     CLOSE = 0,
     SUCCESS = 1,
-    ERROR = 2
+    ERROR = 2,
+    SERVER_SHUTDOWN = 3,
+    WARNING = 4
 
-async def handle_leaving(websocket):
+async def handle_leaving(websocket, lost_connection=False):
     client_data = connected_clients[websocket]
+    del connected_clients[websocket]
+
+    if lost_connection:
+        message = message_module.Message('TEXT', f'{client_data["name"]} lost connection.').pack()
+    else:
+        message = message_module.Message('TEXT', f'{client_data["name"]} left the server.').pack()
+
     for client in connected_clients.keys():
-        if client != websocket:
-            await client.send(message_module.Message('TEXT', f'{client_data["name"]} left the server.').pack())
+        await client.send(message)
+
+async def handle_command(websocket, path, client_data, msg, command, params):
+    if command == 'shutdown':
+        return Status.SERVER_SHUTDOWN, "Shutting down the server..."
+
+    elif command == 'setname':
+        old_name = client_data['name']
+        new_name = ' '.join(params)
+        client_data['name'] = new_name
+        result = f'Setting name of {old_name} to {new_name}.'
+        for client in connected_clients.keys():
+            await client.send(message_module.Message('TEXT', result).pack())
+        return Status.SUCCESS, result
+
+    return Status.WARNING, "Command not recognized."
 
 async def handle_message(websocket, path, client_data, msg):
     if msg.message_type == 'CONNECTION_CLOSE':
         return Status.CLOSE, f'{client_data["name"]} left the server.'
-    elif msg.message_type == 'SET_NAME':
-        old_name = client_data['name']
-        new_name = msg.data
-        client_data['name'] = new_name
-        return Status.SUCCESS, f'Setting name of {old_name} to {new_name}.'
+
+    elif msg.message_type == 'COMMAND':
+        msg.data = msg.data[len(command_prefix):]
+        parts = msg.data.split(' ')
+        command = parts[0]
+        params = parts[1:]
+        return await handle_command(websocket, path, client_data, msg, command, params)
+
     elif msg.message_type == "HELLO":
         await websocket.send(message_module.Message('TEXT', 'Welcome to the server!').pack())
         return Status.SUCCESS, f'{client_data["name"]} joined the server.'
@@ -42,19 +70,29 @@ async def response(websocket, path):
             ip = websocket.remote_address[0]
             port = websocket.remote_address[1]
 
-            client_data = {'join_time': time.time(), 'ip': ip, 'port': port, 'name': f'{ip} [{port}]'}
+            client_data = {'join_time': time.time(), 'ip': ip, 'port': port, 'name': f'Unknown{len(connected_clients)}'}
             connected_clients[websocket] = client_data
             logger.info(f'Client connected: {client_data["name"]}')
 
             while True:
-                packed_message = await websocket.recv()
+                try:
+                    packed_message = await websocket.recv()
+                except (websockets.exceptions.ConnectionClosedError, websockets.exceptions.ConnectionClosedOK) as e:
+                    logger.info(f'Lost connection with {client_data["name"]}')
+                    await handle_leaving(websocket, lost_connection=True)
+                    break
+                except Exception as e:
+                    raise e                    
+
                 message = message_module.Message.from_packed(packed_message)
                 logger.info(f'Message from {client_data["name"]}, message_type: {message.message_type}, msg: {message.data}')
 
                 try:
+                    if message.message_type == "TEXT" and message.data.startswith(command_prefix):
+                        message.message_type = "COMMAND"
+
                     status, result = await handle_message(websocket, path, client_data, message)
-                    if result:
-                        logger.info(result)
+                    logger.info(f'MESSAGE_HANDLER: status: {status.name}, result: {result}')
                 except Exception as e:
                     logger.error(f'Exception: {e}')
                     status, result = Status.ERROR, None
@@ -72,18 +110,21 @@ async def response(websocket, path):
                         packed_message = message.pack()
                         for client in connected_clients.keys():
                             await client.send(packed_message)
-                    elif message.message_type in ['SET_NAME', 'HELLO']:
+                    elif message.message_type == 'HELLO':
                         for client in connected_clients.keys():
                             await client.send(message_module.Message('TEXT', result).pack())
+                elif status == Status.WARNING:
+                    # logger.warn(result)
+                    pass
+                elif status == Status.SERVER_SHUTDOWN:
+                    raise NotImplementedError('Shutdown not implemented')
 
         else:
             logger.warn(f"Client with that IP and port is already connected")
             await websocket.send(message_module.Message('TEXT', "Client with that IP and port is already connected").pack())
-
     finally:
         if websocket in connected_clients:
             await handle_leaving(websocket)
-            del connected_clients[websocket]
 
         await websocket.close()
 
